aa7a874000335766dd3161a0d05fa01b97069d89
jkivlen <jimmykivlen@gmail.com>
2020-12-21 23:03:09 -0600

    added and migrated user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201221204418) do
+ActiveRecord::Schema.define(version: 20201222044959) do
 
   create_table "movies", force: :cascade do |t|
     t.string  "name"
     t.integer "year"
     t.string  "genre"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
