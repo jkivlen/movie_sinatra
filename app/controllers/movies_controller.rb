65a66672301b8bc915d0a3a49257ebeec04d28f5
jkivlen <jimmykivlen@gmail.com>
2020-12-23 09:30:33 -0600

    added everything to views/movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,48 @@
 class MoviesController < ApplicationController
 
-    get "/movies" do #index - loads all the movies
+    get '/movies/new' do
+
+        erb :"movies/new"
+    end
+
+    post '/movies' do
+        movies = Movie.create(params)
+        redirect "/movies"
+
+    end
+
+    get "/movies" do           #index - loads all the movies
         @movies = Movie.all
         
         erb :"movies/index"
     end
 
+    get '/movies/:id' do
+        @movie = Movie.find_by(id: params[:id])
+        if @movie
+            erb :"movies/show"
+        else
+            redirect "/movies"
+    end
+
+   get '/movies/:id/edit' do
+        @movie = Movie.find_by(id: params[:id])
+
+
+    erb :"movies/edit"
+   end
+
+   patch "/movies/:id/edit" do
+    @movie = Movie.find_by(id: params[:id])
+    @movie.update(params[:movie])
+    redirect to "/movies/#{@movie.id}"
+   end
+
+   delete "/movies/:id" do
+        @movie = Movie.find_by(id: params[:id])
+        @movie.destroy
+        redirect to "/movies"
+   end
 
 
 end
