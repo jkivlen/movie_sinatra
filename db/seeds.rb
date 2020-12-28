0bf1e267408ca23b9c74548fdfce670a8033591f
jkivlen <jimmykivlen@gmail.com>
2020-12-27 18:31:56 -0600

    added user to show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
 Movie.create(name: "The Matrix", year: 1999, genre: "Action")
-Movie.create(name: "Old School", year: 2003, genre: "Comedy")
-Movie.create(name: "Boogie Nights", year: 1997, genre: "Drama")
-Movie.create(name: "Goofellas", year: 1990, genre: "Crime")
